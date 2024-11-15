be39c11f5dea06dbde0fabe45c7660ff3b911952
sktomsi <designsbytomc@gmail.com>
2024-11-15 15:22:36 +0000

    🖼  add gallery page (fixed build errors)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,13 +9,3 @@ export const easings = {
   easeOutQuart: [0.25, 1, 0.5, 1],
   easeInOutQuint: [0.83, 0, 0.17, 1],
 };
-
-export function importAll(r) {
-  const images = {};
-  r.keys().forEach((item: string) => {
-    // Get the image name without extension as the key
-    const imageName = item.replace('./', '').split('.')[0];
-    images[imageName] = r(item);
-  });
-  return images;
-}
