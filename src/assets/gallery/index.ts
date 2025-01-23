25bb5889d4b5806e55accbadbb730a5f5ab08a27
sktomsi <designsbytomc@gmail.com>
2025-01-23 11:14:08 +0000

    add new images

--- src/assets/gallery/index.ts
+++ src/assets/gallery/index.ts
@@ -1,29 +1,23 @@
 import Commercial1 from '@/assets/gallery/commercial/2017-03-29-PHOTO-00000402.webp';
 import Commercial2 from '@/assets/gallery/commercial/2017-03-29-PHOTO-00000405.webp';
 import Commercial3 from '@/assets/gallery/commercial/2017-03-29-PHOTO-00000406.webp';
-import Commercial4 from '@/assets/gallery/commercial/2017-03-29-PHOTO-00000408.webp';
 import Commercial5 from '@/assets/gallery/commercial/2017-03-29-PHOTO-00000409.webp';
 import Commercial7 from '@/assets/gallery/commercial/Cover Photo.webp';
-import Commercial9 from '@/assets/gallery/commercial/samara.webp';
+import Commercial4 from '@/assets/gallery/commercial/IMG_8228.webp';
 import Commercial10 from '@/assets/gallery/commercial/samsara 101.webp';
-import Commercial8 from '@/assets/gallery/commercial/SDC12662.webp';
 import Hospitality1 from '@/assets/gallery/hospitality/2017-08-29-PHOTO-00000640.webp';
-import Hospitality2 from '@/assets/gallery/hospitality/2017-08-29-PHOTO-00000643.webp';
-import Hospitality5 from '@/assets/gallery/hospitality/2017-08-29-PHOTO-00000644.webp';
 import Hospitality3 from '@/assets/gallery/hospitality/2017-08-29-PHOTO-00000645.webp';
 import Hospitality6 from '@/assets/gallery/hospitality/2017-08-29-PHOTO-00000646.webp';
 import Hospitality7 from '@/assets/gallery/hospitality/IMG_9265.webp';
 import Hospitality8 from '@/assets/gallery/hospitality/IMG_9273.webp';
-import Hospitality9 from '@/assets/gallery/hospitality/IMG_9274.webp';
+import Hospitality2 from '@/assets/gallery/hospitality/Mercure.webp';
 import Residence2 from '@/assets/gallery/residences/4.png';
 import Residence4 from '@/assets/gallery/residences/410_Pavlova_01.webp';
 import Residence5 from '@/assets/gallery/residences/410_Pavlova_04.webp';
-import Residence6 from '@/assets/gallery/residences/410_Sunshine_AB_03.webp';
-import Residence7 from '@/assets/gallery/residences/5.webp';
-import Residence9 from '@/assets/gallery/residences/8.webp';
 import Residence10 from '@/assets/gallery/residences/9.png';
-import Residence12 from '@/assets/gallery/residences/BCAP_01.webp';
 import Residence14 from '@/assets/gallery/residences/Picture4.webp';
+import Residence6 from '@/assets/gallery/residences/SAG_1395-Edit.webp';
+import Residence12 from '@/assets/gallery/residences/SAG_1576-Edit.webp';
 import Hospitality4 from '@/assets/Magarpatta Cocoon.jpg';
 
 export const GalleryImages = {
@@ -35,29 +29,25 @@ export const GalleryImages = {
     Residence12,
     Residence10,
     Residence2,
-    Residence7,
-    Residence9,
+    // Residence7,
+    // Residence9,
   ],
   commercial: [
     Commercial1,
+    Commercial4,
     Commercial2,
     Commercial3,
-    Commercial9,
-    Commercial4,
     Commercial5,
     Commercial7,
-    Commercial8,
     Commercial10,
   ],
   hospitality: [
     Hospitality4,
-    Hospitality5,
     Hospitality1,
     Hospitality2,
     Hospitality3,
     Hospitality6,
     Hospitality7,
     Hospitality8,
-    Hospitality9,
   ],
 };
